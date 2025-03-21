c14ba0611137331fe40d3f720058a6d8d147c1f9
Hugo Mayonobe <hugomayonobe@gmail.com>
2025-03-21 15:38:10 +0100

    Modification de la clé SSH publique dans la configuration de la machine virtuelle Azure pour utiliser une variable au lieu d'un chemin de fichier statique.

--- vm.tf
+++ vm.tf
@@ -11,7 +11,7 @@ resource "azurerm_linux_virtual_machine" "ci-cd-vm" {
 
   admin_ssh_key {
     username = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = var.ssh_public_key
   }
 
   source_image_reference {
